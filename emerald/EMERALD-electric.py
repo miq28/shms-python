a3397c7f9e3f5ffba9c89369be87f55d631276f1
miq28 <iqbal.tiro@gmail.com>
2024-03-30 12:16:21 +0000

    fix bug logging

--- emerald/EMERALD-electric.py
+++ emerald/EMERALD-electric.py
@@ -20,9 +20,40 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 import logging_config
 import logging
 
+class CustomFormatter(logging.Formatter):
+
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    blue = "\x1b[34m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: blue + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
 # logger = logging.getLogger(__name__)
 logger = logging.getLogger(os.path.basename(__file__))
+logger.setLevel(logging.INFO)
 
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+ch.setFormatter(CustomFormatter())
+
+logger.addHandler(ch)
 
 URL = os.getenv("URL_EMERALD")
 TOKEN = os.getenv('TOKEN_EMERALD')
---
+++ hikconnect.py
@@ -0,0 +1,69 @@
+import asyncio
+import hikconnect
+import hikconnect.api
+
+
+        
+
+
+async def main():    
+    async with HikConnect() as api:
+
+        await api.login("foo", "bar")
+
+        devices = [device async for device in api.get_devices()]
+        print(devices)
+        # [{
+        #   'id': 'DS-XXXXXX-YYYYYYYYYYYYYYYYYYYYYYYYY',
+        #   'name': 'DS-XXXXXX-Y(ZZZZZZZZZ)',
+        #   'serial': 'ZZZZZZZZZ',
+        #   'type': 'DS-XXXXXX-Y',
+        #   'version': 'V1.2.3 build 123456',
+        #   'locks': {1: 2, 2: 0, 3: 1}
+        # }]
+        # locks data means (guessing): <channel number>: <number of locks connected>
+
+        my_device_serial = devices[0]["serial"]
+
+        cameras = [camera async for camera in api.get_cameras(my_device_serial)]
+        print(cameras)
+        # [
+        #   {'id': '4203fd7c5f89ce96f8ff0adfdbe8b731', 'name': 'foo', 'channel_number': 1, 'signal_status': 1, 'is_shown': 0},
+        #   {'id': 'cd72bc923956952194468738123b7a5e', 'name': 'bar', 'channel_number': 2, 'signal_status': 1, 'is_shown': 1},
+        #   {'id': 'd2a2057d853438d9a5b4954baec136e3', 'name': 'baz', 'channel_number': 3, 'signal_status': 0, 'is_shown': 0}
+        # ]
+
+        call_status = await api.get_call_status(my_device_serial)
+        print(call_status)
+        # {
+        #   'status': 'idle',
+        #   'info': {
+        #     'building_number': 0,
+        #     'floor_number': 0,
+        #     'zone_number': 0,
+        #     'unit_number': 0,
+        #     'device_number': 0,
+        #     'device_type': 0,
+        #     'lock_number': 0
+        #   }
+        # }
+        # can be "idle" / "ringing" / "call in progress" - see hikconnect/api.py:45
+        
+        # Unlock device
+        await api.unlock(my_device_serial, 1)
+        
+        # Cancel call for device
+        await api.cancel_call(my_device_serial)
+
+        # call this periodically at least once per 30 mins!
+        if api.is_refresh_login_needed():
+            await api.refresh_login()
+
+       
+try:
+    # print('%s started' % os.path.basename(__file__))
+    # logger.info('%s started', os.path.basename(__file__))
+    asyncio.run(main())
+except KeyboardInterrupt:
+    print('shutting down')
+    # logger.info('KeyboardInterrupt, shutting down...')
